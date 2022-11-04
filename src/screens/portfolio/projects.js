5f64a4df43eee882247692d178409a2c5b31bcd2
Kosta-brillakis <78456468+Kosta-brillakis@users.noreply.github.com>
2022-11-04 12:03:13 -0700

    added concert site

--- src/screens/portfolio/projects.js
+++ src/screens/portfolio/projects.js
@@ -1,8 +1,15 @@
 import pokemon from './images/pokemon.jpg'
 import quoteApp from './images/quoteApp.jpg'
 import reactTodo from './images/react-todolist.png'
+import concertSite from './images/website.png'
 
 export const projects = [
+    {
+        title: 'Concert Venue Website',
+        description: 'Full home page for a concert venue. Complete with a responsive navbar, artist carousel, event timeline, and pricing table. Includes animations, and made from scratch using Tailwind CSS.',
+        image: `${concertSite}`,
+        url: 'https://kostas-concert-site.netlify.app'
+    },
     {
         title: "React Todo List",
         description: "Small yet tricky todo list built with react and including full local storage functionality",
